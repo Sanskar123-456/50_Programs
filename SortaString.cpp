c06f76192ab2b3b1cd553a1d911e77ec3d73cf00
Sanskar Chaurasia <sanskarchaurasia456@gmail.com>
2025-04-17 21:30:12 +0530

    Sort

--- SortaString.cpp
+++ SortaString.cpp
@@ -1,23 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s1="acdebba";
-    int n=s1.size();
-    vector<int>s(26,0);
-    int count,ind;
-    count=1;
-    for(int i=0;i<n;i++){
-        s[s1[i]-'a']++;
-    }
-    string s2;
-    for(int i=0;i<26;i++){
-        char c='a'+i;
-        while(s[i]){
-            s2+=c;
-            s[i]--;
-        }
-    }
-    for(int i=0;i<n;i++){
-        cout<<s2[i];
-    }
+
+    string str="ABC\0DEF";
+    cout<<str;
+    // string s1="acdebba";
+    // int n=s1.size();
+    // vector<int>s(26,0);
+    // int count,ind;
+    // count=1;
+    // for(int i=0;i<n;i++){
+    //     s[s1[i]-'a']++;
+    // }
+    // string s2;
+    // for(int i=0;i<26;i++){
+    //     char c='a'+i;
+    //     while(s[i]){
+    //         s2+=c;
+    //         s[i]--;
+    //     }
+    // }
+    // for(int i=0;i<n;i++){
+    //     cout<<s2[i];
+    // }
 }
